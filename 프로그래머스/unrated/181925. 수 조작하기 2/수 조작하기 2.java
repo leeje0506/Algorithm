a341706b56d1c0675d9e77b638a178e068d5af0c
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2023-09-01 15:13:38 +0900

    [unrated] Title: 수 조작하기 2, Time: 651.16 ms, Memory: 378 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,21 +1,6 @@
 class Solution {
     public String solution(int[] numLog) {
         String answer = "";
-        
-        for(int i=1; i<numLog.length; i++){
-            if(numLog[i] - numLog[i-1]==1){
-                answer += "w";
-            }
-            else if(numLog[i] - numLog[i-1]==10){
-                answer += "d";
-            }
-            if(numLog[i] - numLog[i-1]==-1){
-                answer += "s";
-            }
-            if(numLog[i] - numLog[i-1]==-10){
-                answer += "a";
-            }
-        }
         return answer;
     }
 }
