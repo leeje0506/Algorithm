bcec819e06670747d111b4c6a57b7a04837fc1f2
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2023-09-02 14:07:07 +0900

    [unrated] Title: n 번째 원소까지, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int [n];
+        
+        
+        for(int j=0; j<n; j++){
+            answer[j] = num_list[j];
+        }
+            
+        
+        return answer;
+    }
+}
