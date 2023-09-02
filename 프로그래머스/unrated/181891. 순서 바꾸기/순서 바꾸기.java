18e51d8c2275827f0835608c6a75ec1c8b6d0a31
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2023-09-02 19:57:47 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.01 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length];
+        int index = 0;
+        for (int i = n; i < num_list.length; i++) {
+            answer[index++] = num_list[i];
+        }
+        //index는 앞서 증가한 값이 유지되고 있으므로, answer 배열에 연이어 원소가 채워짐
+        for (int i = 0; i < n; i++) {
+            answer[index++] = num_list[i];
+        }
+        return answer;
+    }
+}
