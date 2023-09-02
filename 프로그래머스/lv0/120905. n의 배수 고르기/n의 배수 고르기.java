3db8dce2cc418c2ed4353d5ec7ff4b2f6e423dd6
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2023-09-02 20:18:22 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.01 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        int[] arr = new int[numlist.length];
+        int cnt = 0;
+        for (int i = 0; i < numlist.length; i++) {
+            if (numlist[i] % n == 0) {
+                arr[cnt++] = numlist[i];
+            }
+        }
+        int[] answer = new int[cnt];
+        for (int i = 0; i < cnt; i++) {
+             answer[i] = arr[i];
+        }
+        return answer;
+    }
+}
