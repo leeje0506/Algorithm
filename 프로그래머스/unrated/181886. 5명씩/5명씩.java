1017f0ff939cc96eaf36b84c9ff526353e42859e
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2023-09-02 19:51:53 +0900

    [unrated] Title: 5명씩, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String[] solution(String[] names) {
+        int len =0; 
+        if(names.length%5==0){
+            len = (names.length/5);  
+        }        
+        else if(names.length%5!=0){ 
+            len = ((names.length/5) +1);
+        }
+        
+        String[] answer = new String [len];
+        
+        int idx=0;
+        
+        for(int i=0; i<names.length; i+=5){
+            answer[idx++]=names[i];
+        }
+        return answer;
+    }
+}
