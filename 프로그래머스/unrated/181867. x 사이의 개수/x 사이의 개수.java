472aaa5a3b037c8402d76a8375dda742a0005c71
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2023-09-04 02:09:29 +0900

    [unrated] Title: x 사이의 개수, Time: 2.89 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(String myString) {
+        //-1은 구분자를 기준으로 구분할 때, 빈 문자열까지 포함하여 반환한다는 뜻
+        String[] s = myString.split("x", -1);
+        
+        int[] answer = new int[s.length];
+        
+        for (int i = 0; i < s.length; i++) {
+            //answer의 i번째 원소 위치에 s의 i번째 원소의 길이만큼 할당
+            answer[i] = s[i].length();
+        }
+        return answer;
+    }
+}
