11ccc96c107f74d1b0ba3676a5da1f5dee57aa18
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2023-09-04 10:34:08 +0900

    [level 0] Title: 약수 구하기, Time: 0.03 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        
+        
+        ArrayList<Integer> list = new ArrayList<>(); 
+        
+        for(int i=1; i<=n; i++){
+            if(n%i==0){
+                list.add(i);
+            }
+        }
+        int[] answer = new int[list.size()];
+        int idx = 0;
+        for (int num: list) {
+            answer[idx++] = num;
+        }
+        
+        return answer;
+    }
+}
