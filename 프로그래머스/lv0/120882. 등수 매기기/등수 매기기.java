df24f19ce7d5e4d34ffaec1f27dc72cc0b21ac2a
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2023-09-04 17:22:20 +0900

    [level 0] Title: 등수 매기기, Time: 0.40 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[][] score) {
+        int[] answer = new int [score.length];
+        double [] arr = new double [score.length];
+        double[] arr2 = new double [score.length]; //sort
+        int eng=0;
+        int math=0;
+        double sum=0;
+        
+        for(int r=0; r<score.length; r++){
+            sum = (score[r][0] + score[r][1])/2.0;
+            arr[r] = sum;
+            arr2[r] = sum;
+        }
+        Arrays.sort(arr2);
+        
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length; j++){
+                if(arr[i]==arr2[j]){
+                     answer[i]=arr.length-j;  
+                }
+            }
+        }
+        
+        
+        return answer;
+    }
+}
