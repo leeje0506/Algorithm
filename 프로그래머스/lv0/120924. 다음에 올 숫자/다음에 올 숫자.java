5302f824ed4820e3d989c9d9ea3b2143809d3e31
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2023-09-04 10:52:57 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        //등차
+        if (common[1] - common[0] == common[2] - common[1]) {
+          answer = common[common.length - 1] + (common[1] - common[0]);
+        } 
+        //등비
+        else {
+            answer = common[common.length - 1] * (common[1] / common[0]);
+        }
+        
+        return answer;
+    }
+}
