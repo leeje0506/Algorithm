a9a9afa6c2cd3cab9aa8f1b18edd2978199daae2
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2023-09-18 22:49:47 +0900

    [Silver IV] Title: 큐, Time: 204 ms, Memory: 18896 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,55 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static Deque<Integer> que = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); // 명령의 수
+
+        StringBuilder sb = new StringBuilder();
+        for (int tc = 1; tc <= N; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String s = st.nextToken();
+
+            if (s.equals("push")) {
+                int x = Integer.parseInt(st.nextToken());
+                que.offer(x);
+            } else if (s.equals("pop")) {
+                if (que.size() < 1) { // 큐가 비어있을 때
+                    sb.append(-1).append("\n"); // -1을 추가하고 줄바꿈
+                } else {
+                    sb.append(que.pollFirst()).append("\n"); // 큐에서 값을 꺼내고 추가하고 줄바꿈
+                }
+            } else if (s.equals("size")) {
+                sb.append(que.size()).append("\n"); // 큐의 크기를 추가하고 줄바꿈
+            } else if (s.equals("empty")) {
+                if (que.isEmpty()) {
+                    sb.append(1).append("\n"); // 큐가 비어있으면 1을 추가하고 줄바꿈
+                } else {
+                    sb.append(0).append("\n"); // 큐가 비어있지 않으면 0을 추가하고 줄바꿈
+                }
+            } else if (s.equals("front")) {
+                if (que.isEmpty()) {
+                    sb.append(-1).append("\n"); // 큐가 비어있을 때 -1을 추가하고 줄바꿈
+                } else {
+                    sb.append(que.peekFirst()).append("\n"); // 큐의 맨 앞 값을 추가하고 줄바꿈
+                }
+            } else if (s.equals("back")) {
+                if (que.isEmpty()) {
+                    sb.append(-1).append("\n"); // 큐가 비어있을 때 -1을 추가하고 줄바꿈
+                } else {
+                    sb.append(que.peekLast()).append("\n"); // 큐의 맨 뒤 값을 추가하고 줄바꿈
+                }
+            }
+        } // tc for문
+
+        System.out.println(sb.toString()); // 결과를 출력
+    }
+}
