f15dda12ab93878fcaf7f0ec525a678c268a7938
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2023-09-20 20:56:50 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 194 ms, Memory: 31,488 KB -BaekjoonHub

---
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -0,0 +1,72 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	static int idx, num;
+	static List<Integer> order ;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		for(int tc =1; tc<=10; tc++) {
+			//원본 암호문의 길
+			int N = Integer.parseInt(br.readLine());
+
+			
+			order = new LinkedList<>();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int i=0; i<N; i++) {
+				order.add(Integer.parseInt(st.nextToken()));
+			}
+			
+			//명령어의 개수 
+			int M = Integer.parseInt(br.readLine()); 
+			
+			
+			st = new StringTokenizer(br.readLine());
+			
+			for(int i=0; i<M; i++) {
+				String in = st.nextToken(); //삽입
+				if(in.equals("I")) {
+					idx = Integer.parseInt(st.nextToken()); //삽입 위치 : idx
+					num = Integer.parseInt(st.nextToken()); //몇 개의 숫자를 삽입하는지
+					
+					for(int j=0; j<num; j++) {
+						order.add(idx, Integer.parseInt(st.nextToken()));
+						idx++;
+					}
+					
+				}
+				
+				else if (in.equals("D")) {
+					idx = Integer.parseInt(st.nextToken()); //삭제 위치 
+					num = Integer.parseInt(st.nextToken()); //몇 개의 숫자를 삭제 
+					
+					for(int j=0; j<num; j++) {
+						order.remove(idx);
+						idx++;
+					}
+					
+				}
+				else if(in.equals("A")) {
+					num = Integer.parseInt(st.nextToken()); //몇 개의 숫자를 삽입하는지
+					
+					for(int j=0; j<num; j++) {
+						order.add(Integer.parseInt(st.nextToken()));
+					}
+				}
+			}
+			System.out.print("#" + tc);
+			for (int t = 0; t < 10; t++) {
+				System.out.print(" " + order.get(t));
+			}
+			System.out.println();
+		}
+
+	}//main
+}//class
