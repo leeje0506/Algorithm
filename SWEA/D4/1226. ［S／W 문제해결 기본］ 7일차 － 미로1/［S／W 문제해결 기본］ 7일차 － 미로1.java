ef1f1ad5946d555d7530a954892a794962c90bf3
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2023-09-24 22:37:26 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 110 ms, Memory: 18,580 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
@@ -5,67 +5,57 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
 public class Solution {
-	static int [] dr = {-1,1,0,0};
-	static int [] dc = {0,0,1,-1};
-	static int [][] maze = new int[16][16];
-	static int [][] visited = new int [16][16];
-	static int answer=0;
-	public static void main(String[] args) throws IOException{
+	//갈 수 있는지만 보면 됨  > dfs 사용
+	//범위 잘 주기
+	//종료 조건 
+	
+	public static int [][] map;
+	public static boolean [][] visit;
+	public static int [] dr = {-1,1,0,0,};
+	public static int [] dc = {0,0,1,-1};
+	public static int answer;
+	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		for(int tc =1; tc<=10; tc++) {
-			int T = Integer.parseInt(br.readLine());
-		
+		for(int tc=1; tc<=10; tc++) {
+			int T = Integer.parseInt(br.readLine()); //테케 번호
 			
-			maze = new int [16][16];
-			visited = new int [16][16];
-			answer =0;
+			map = new int[16][16];
+			visit = new boolean[16][16];
 			
 			for(int r=0; r<16; r++) {
-				String line = br.readLine();
-//				String str = br.readLine();
-//				StringTokenizer st = new StringTokenizer(str, "");
-				
-				for(int c=0;c<16; c++) {
-					maze[r][c] = line.charAt(c) - '0';
+				StringTokenizer st = new StringTokenizer(br.readLine(), "1||0", true);
+				for(int c=0; c<16; c++) {
+					map[r][c] = Integer.parseInt(st.nextToken());
 				}
-			}
+			}//입력 끝
 			
-			
+			answer=0;
 			dfs(1,1);
-			
-			
 			System.out.println("#" + tc + " " + answer);
-		}
-		
+		}//tc for
 	}//main
 	
-	//bfs
-	public static void dfs(int r, int c) {
-//		visited[r][c] = false;
-		
-		//3 : 도착 지점  
-		if(maze[r][c] == 3) {
-			answer =1;
+	public static void dfs (int s, int e) {
+        //3 도착지점. 도착하면 answer=1 반환하고 종료.
+		if(map[s][e]==3) {
+			answer = 1;
 			return;
 		}
-//		if(answer ==1) {
-//			return;
-//		}
-//		visited[r][c] = true;
-		visited[r][c] = 1;
+        //방문 완~
+		visit[s][e]=true;
 		
 		
-		for(int i=0; i<4; i++) {
-			int nr = r+dr[i];
-			int nc = c+dc[i];
-			if(nr>=0 && nr < 16 && nc>=0 && nc<16 && visited[nr][nc]==0 && maze[nr][nc]!=1) {
-				dfs(nr, nc);	
+		for(int d=0; d<4; d++) {
+			int nr = s + dr[d];
+			int nc = e + dc[d];
+            //범위 안에 있고, 방문처리 되어있지 않으면서, 1(벽)이 아닌 곳만 탐색
+			if(nr>=0 && nr < 16 && nc>=0 && nc<16 && visit[nr][nc]== false && map[nr][nc]!=1) {
+				dfs(nr, nc);
 			}
 		}
-		visited[r][c] = 0;
+        //탐색이 끝났으면 방문처리 다시 false
+		visit[s][e] = false;
 	}//bfs
-
 }//class
