0131f8a5226fc8af0d24caee2ee420ff7a6894f1
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2023-11-01 22:53:03 +0900

    [Gold V] Title: 괄호의 값, Time: 132 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.java
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -1,5 +1,5 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
@@ -9,46 +9,35 @@ public class Main {
 
     // 재귀 함수로 괄호 쌍 별로 계산하는 함수
     public static int recur(int start, int end) {
-        // 괄호 쌍이 하나만 있는 경우
-        if (end - start == 1) {
-            // 괄호가 '(' 이면 2를 반환
-            if (input.charAt(start) == '(') return 2;
-            // 괄호가 '[' 이면 3을 반환
-            else return 3;
+        if (end - start == 1) {  // 괄호 쌍이 하나만 있는 경우
+            if (input.charAt(start) == '(') return 2;  // '(' 이면 2 반환
+            else return 3;  // '[' 이면 3 반환
         }
 
-        // 괄호가 '(' 이면 곱셈 값으로 2를, 아니면 3을 설정
-        int mul = input.charAt(start) == '(' ? 2 : 3;
+        int mul = input.charAt(start) == '(' ? 2 : 3;  // '(' 이면 2, 아니면 3
         start++;
 
-        // 괄호 쌍 내부의 값을 합산
-        int sum = 0;
+        int sum = 0;  // 괄호 쌍 내부의 값을 합산하는 변수
         while (start <= end - 1) {
             int pairIdx = pair[start];
             sum += recur(start, pairIdx);
             start = pairIdx + 1;
         }
 
-        // 내부의 합산 값에 외부 괄호 값 곱하기
-        return sum * mul;
+        return sum * mul;  // 내부의 합산 값에 외부 괄호 값 곱하기
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean incorrect = false;  // 괄호 문자열의 올바름 여부를 판단하는 변수
 
-        // 괄호 문자열의 올바름 여부를 판단하는 변수
-        boolean incorrect = false;  
+        input = br.readLine();
 
-        input = sc.next();
-
-        // 괄호의 올바름 여부를 확인하는 스택
-        Stack<Character> validCheck = new Stack<>();
-        // 괄호의 인덱스를 저장하는 스택
-        Stack<Integer> stack = new Stack<>();
+        Stack<Character> validCheck = new Stack<>();  // 괄호의 올바름 여부를 확인하는 스택
+        Stack<Integer> stack = new Stack<>();  // 괄호의 인덱스를 저장하는 스택
 
         pair = new int[input.length()];
 
-        // 입력받은 문자열을 순회하며 괄호의 짝을 찾음
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
             if (c == '(') {
@@ -58,8 +47,7 @@ public class Main {
                 validCheck.add(']');
                 stack.add(i);
             } else {
-                // 스택이 비어있는데 pop을 시도하는 경우를 방지
-                if(validCheck.isEmpty()) { 
+                if(validCheck.isEmpty()) {  // 스택이 비어있는데 pop을 시도하는 경우를 방지
                     incorrect = true;
                     break;
                 }
@@ -74,12 +62,10 @@ public class Main {
             }
         }
 
-        // 괄호 문자열이 올바르지 않은 경우 0 출력
         if (!validCheck.isEmpty() || incorrect) {
-            System.out.println(0);
+            System.out.println(0);  // 괄호 문자열이 올바르지 않은 경우 0 출력
         } else {
-            // 올바른 괄호 문자열인 경우 각 괄호 쌍 별로 계산
-            int sum = 0;
+            int sum = 0; 
             int start = 0;
             int end = input.length() - 1;
             
@@ -90,8 +76,7 @@ public class Main {
                 start = pairIdx + 1;
             }
             
-            // 최종 결과 출력
-            System.out.println(sum);
+            System.out.println(sum);  // 최종 결과 출력
         }
     }
 }
