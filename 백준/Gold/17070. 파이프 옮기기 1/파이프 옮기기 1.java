a893cae0287b8c5db7004f200bcd71390be77f6e
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2023-11-09 14:11:08 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 208 ms, Memory: 16988 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,53 +1,76 @@
-import java.io.*;
-import java.util.*;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int[][] map;
-    static int count = 0;
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	static int N;
+	static int [][] map;
+	static int cnt = 0;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		map = new int[N+2][N+2];
+		
+		for(int r=1; r<N+1; r++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int c=1; c<N+1; c++) {
+				map[r][c]=Integer.parseInt(st.nextToken());
+				
+			}
+		}//map 입력
+		
+		//가장 처음에 파이프는 (1, 1)와 (1, 2)를 차지하고 있다
+		//시작은 r=1, c=2, dir=0
+		dfs(1, 2, 0);
+		
+		System.out.println(cnt);
+		
+	}//main
 
-        N = Integer.parseInt(br.readLine());
-        map = new int[N+1][N+1];
-
-        for(int i = 1; i<=N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
-            for(int j = 1; j<=N; j++)
-                map[i][j] = Integer.parseInt(st.nextToken());
-        }
-
-        dfs(1, 2, 0);
-
-        System.out.println(count);
-    }
-
-    //dir이 0이면 가로 1이면 세로 2면 대각선
-    static void dfs(int row, int col, int dir){
-        if(row>N || col>N || map[row][col] == 1)
+	 //dir이 0이면 가로 1이면 세로 2면 대각선
+	public static void dfs(int r, int c, int dir) {
+		
+		if(r>N || c>N || map[r][c] == 1)
             return;
+		
+		
+        
+		
+		//대각선
         if(dir == 2){
-            if(map[row][col-1] == 1 || map[row-1][col] == 1)
+        	//파이프 끝 점에 벽이 있으면 리턴
+            if(map[r][c-1] == 1 || map[r-1][c] == 1)
                 return;
         }
-        if(row == N && col == N){
-            count++;
+        
+      //끝 지점에 도달하면 cnt++
+      	if(r == N && c == N){
+      		cnt++;
             return;
         }
-
+        
         if(dir == 0){
-            dfs(row, col+1, 0);
-            dfs(row+1, col+1, 2);
+            dfs(r, c+1, 0);
+            dfs(r+1, c+1, 2);
         }
+        
         else if(dir == 1){
-            dfs(row+1, col, 1);
-            dfs(row+1, col+1, 2);
+            dfs(r+1, c, 1);
+            dfs(r+1, c+1, 2);
         }
+        
         else{
-            dfs(row+1, col+1, 2);
-            dfs(row, col+1, 0);
-            dfs(row+1, col, 1);
+            dfs(r+1, c+1, 2);
+            dfs(r, c+1, 0);
+            dfs(r+1, c, 1);
         }
-    }
-}
+		
+	}//dfs
+	
+}//class
