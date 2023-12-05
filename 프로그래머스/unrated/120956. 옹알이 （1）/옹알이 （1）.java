6b505c31f9f2f443ce9c329d3460f791bd6b0cba
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2023-12-06 03:29:25 +0900

    [level unrated] Title: 옹알이 (1), Time: 0.53 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for (int i = 0; i < babbling.length; i++) {
+            if (babbling[i].contains("aya") || babbling[i].contains("ye") || babbling[i].contains("woo") || babbling[i].contains("ma")) {
+                babbling[i] = babbling[i].replaceAll("aya|ye|woo|ma", "");
+            }
+            if (babbling[i].equals("")) {
+                answer++;
+            }
+        }
+        return answer; 
+    }
+}
