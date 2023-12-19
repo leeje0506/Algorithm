67b992648af8a7de6cb6a1ed2c10a5ac89e4d82d
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2023-12-19 20:22:42 +0900

    [level unrated] Title: 2의 영역, Time: 1.25 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/unrated/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.java
@@ -6,7 +6,7 @@ class Solution {
         
         int start=0;
         int end =0;
-        int cnt=0;
+        int cnt=0; 
         
         for(int i=0; i<arr.length; i++){
             if(arr[i]==2){
