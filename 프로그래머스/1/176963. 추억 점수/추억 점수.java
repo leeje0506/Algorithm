a50b75777319a50786dd2b206b05f8d3d0589ea0
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2024-01-03 17:43:55 +0900

    [level 1] Title: 추억 점수, Time: 0.03 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] result = new int[photo.length];
+        
+        for (int i = 0; i < photo.length; i++) {
+            int totalScore = 0; 
+            for (int j = 0; j < photo[i].length; j++) {
+                for (int k = 0; k < name.length; k++) {
+                    if (photo[i][j].equals(name[k])) {
+                        totalScore += yearning[k];
+                        break;    
+                    }
+                }
+            }
+            result[i] = totalScore;
+        }
+        
+        return result;
+    }
+}
