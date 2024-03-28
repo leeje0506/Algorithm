9f29946b722ef30be74dbf2d32cd84adc7ea4016
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2024-03-28 21:03:18 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 120 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,42 +1,34 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] height = new int[9];
+        int total = 0;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int [] nanjangee = new int [9];
-		
-		int sum=0;
-		for(int i=0; i<9; i++) {
-			nanjangee[i] = Integer.parseInt(br.readLine());
-			sum += nanjangee[i];
-		}
-		
-		
-		sum = sum - 100;
-		outer : for(int i=0; i<9; i++) {
-			for(int j=i+1; j<9; j++) {
-				if(sum!=nanjangee[i] + nanjangee[j]) {
-				}
-				if(sum == nanjangee[i] + nanjangee[j]	) {
-					nanjangee[i] =0;
-					nanjangee[j] =0;
-					break outer;
-				}else {
-					continue;
-				}
-			}
-		}
-		Arrays.sort(nanjangee);
-		
-		for(int i=2; i<9; i++) {
-			System.out.println(nanjangee[i]);
-		}
+        for (int i = 0; i < 9; i++) {
+            height[i] = Integer.parseInt(br.readLine());
+            total += height[i];
+        }
 
-	}//main
-}//class
+        Arrays.sort(height);
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                //전체에서 두 난쟁이의 키를 뺐을 때, 100이 되는 조합을 찾는 로직
+                if (total - height[i] - height[j] == 100) {
+                    for (int k = 0; k < 9; k++) {
+                        //진짜가 아닌 두 난쟁이를 제외하고 출력
+                        if (k == i || k == j) continue;
+                        System.out.println(height[k]);
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
