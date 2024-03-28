69bd5b9bf7b77466674184a10aace0567fe29573
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2024-03-28 20:55:03 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,15 +1,19 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner (System.in);		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int [] nanjangee = new int [9];
 		
 		int sum=0;
 		for(int i=0; i<9; i++) {
-			nanjangee[i] = sc.nextInt();
+			nanjangee[i] = Integer.parseInt(br.readLine());
 			sum += nanjangee[i];
 		}
 		
