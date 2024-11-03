cba8d31719c09c0e328366e251551d441f65b344
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2024-11-03 21:17:40 +0900

    [Silver II] Title: 촌수계산, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -1,69 +1,92 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
+import java.util.Queue;
+
+/*
+조건 :
+부모 자식 사이 1촌
+
+입력 :
+n  (전체 사람의 수)
+a b (촌수를 계산해야하는 두사람)
+m (부모 쟈식들 간의 관계 개수)
+x y (부모자식 관계 m개, x:부모 y:자식)
+
+출력 :
+a b 의 촌수.
+(친척 관계가 아니라면 -1)
+ */
 
 public class Main {
+    static ArrayList<ArrayList<Integer>> graph;
+    static boolean[] visited;
 
-	public static int N, M, s, e, p, k, answer, cnt = 0;
-	public static int [][] map;
-	public static boolean [] check;
-	public static void main(String[] args)throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-				
-		N = Integer.parseInt(br.readLine()); //전체 사람 수 (정점의 개수)
-		
-		map = new int [N+1][N+1];
-//		check = new boolean[N+1];
-		
-		//촌수를 계산해야하는 두 사람 정보
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		s = Integer.parseInt(st.nextToken()); //시작 노드
-		e = Integer.parseInt(st.nextToken()); //끝 노드
-		
-		M = Integer.parseInt(br.readLine()); //관계 수 (간선의 개수)
-		
-		//부모 자식간의 관계
-		for(int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			p = Integer.parseInt(st.nextToken()); //부모
-			k = Integer.parseInt(st.nextToken()); //자식
-			
-            //사이를 나타내기 때문에 방향 상관없음
-			map[p][k] = map[k][p] =1;
-		}
-		
-		check = new boolean[N+1];
-		
-		dfs(s,e,0);
-	
-		if(answer == 0) {
-			System.out.println(-1);
-		}else {
-			System.out.println(answer);
-		}
-		
-		
-	}//main
-	
-	
-	
-	public static void dfs (int s, int e, int cnt) {
-		check[s] = true;
-		
-		if(s == e) {
-			answer = cnt;
-			return;
-		}
-		for(int i=0; i<=N; i++ ) {
-			if(!check[i] && map[s][i]==1) {
-				check[i] = true;
-				dfs(i, e, cnt+1);
-				check[i] = false;
-			}
-		}
-	}//dfs
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-}//class
+        // 전체 사람 수
+        int n = Integer.parseInt(br.readLine());
+
+        // 촌수를 계산할 두 사람의 번호
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        // 부모-자식 관계의 개수
+        int m = Integer.parseInt(br.readLine());
+
+        // 그래프 초기화
+        graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        // 부모-자식 관계를 입력받아 그래프 구성
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            graph.get(x).add(y);
+            graph.get(y).add(x);  // 양방향 그래프
+        }
+
+        // 방문 배열 초기화
+        visited = new boolean[n + 1];
+
+        // BFS로 촌수 계산 후 결과 출력
+        int result = bfs(start, end);
+        System.out.println(result);
+    }
+
+    public static int bfs(int start, int end) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {start, 0});  // 시작 노드와 촌수 0을 큐에 삽입
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int person = current[0];
+            int degree = current[1];
+
+            // 목표 노드에 도달하면 촌수를 반환
+            if (person == end) {
+                return degree;
+            }
+
+            // 방문하지 않은 이웃 노드를 탐색
+            for (int neighbor : graph.get(person)) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.offer(new int[] {neighbor, degree + 1});  // 촌수 1 증가
+                }
+            }
+        }
+
+        // 도달할 수 없으면 -1 반환
+        return -1;
+    }
+}
