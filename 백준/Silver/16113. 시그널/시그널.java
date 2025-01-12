0ba3d8abd43da3c31a371bfbbc31e547b1d6d8ce
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2025-01-12 13:06:38 +0900

    [Silver I] Title: 시그널, Time: 136 ms, Memory: 20564 KB -BaekjoonHub

--- 백준/Silver/16113. 시그널/시그널.java
+++ 백준/Silver/16113. 시그널/시그널.java
@@ -1,11 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
-    static String result = "";
+    static StringBuilder result = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
 
@@ -18,31 +17,31 @@ public class Main {
         String st = br.readLine();
         String[][] signal = new String[r][c];
 
-        int k=0;
+        int k = 0;
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
                 signal[i][j] = String.valueOf(st.charAt(k));
                 k++;
             }
         }
-        
+
         for (int i = 0; i < c; i++) {
             if (signal[0][i].equals("#")) {
-                if (i == c - 1 ) {
-                    result += "1";
+                if (i == c - 1) {
+                    result.append("1"); 
                     continue;
                 }
                 if (!signal[0][i + 1].equals("#")) {
-                    // 0또는 4 (다음 열이 #이 아님)
+                    // 0 또는 4 (다음 열이 #이 아님)
                     if (signal[3][i].equals("#")) {
                         // 3행이 #이므로 암호는 1
-                        result += "1";
+                        result.append("1");
                     } else {
-                        result += "4";
+                        result.append("4");
                         i += 3;
                     }
                 } else {
-                    // 0,4 제외
+                    // 0, 4 제외
                     if (signal[1][i].equals("#") && signal[1][i + 2].equals("#")) {
                         check_0_8_9(i, signal);
                     } else if (!signal[1][i].equals("#") && signal[1][i + 2].equals("#")) {
@@ -55,38 +54,38 @@ public class Main {
             }
         }
 
-        System.out.println(result);
+        System.out.println(result.toString());
     }
 
     static public void check_0_8_9(int i, String[][] signal) {
         if (!signal[2][i + 1].equals("#")) {
-            result += "0";
+            result.append("0");
         } else {
             if (signal[3][i].equals("#")) {
-                result += "8";
+                result.append("8");
             } else {
-                result += "9";
+                result.append("9");
             }
         }
     }
 
     static public void check_2_3_7(int i, String[][] signal) {
         if (!signal[2][i].equals("#")) {
-            result += "7";
+            result.append("7"); 
         } else {
             if (signal[3][i].equals("#")) {
-                result += "2";
+                result.append("2");
             } else {
-                result += "3";
+                result.append("3");
             }
         }
     }
 
     static public void check_5_6(int i, String[][] signal) {
         if (!signal[3][i].equals("#")) {
-            result += "5";
+            result.append("5"); 
         } else {
-            result += "6";
+            result.append("6");
         }
     }
 }
